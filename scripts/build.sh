6402537611530a2520850af910566f7ecfda4529
shuwen <1243889238@qq.com>
2024-11-18 14:35:32 +0800

    update: 解决指令报错

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 set -e
 echo "正在执行所有包的 build 命令..."
 
-pnpm --filter @arvin/microcode-types\
+pnpm  --filter @arvin/microcode-types\
   --filter @arvin/microcode-theme\
   --filter @arvin/microcode-utils\
   --filter @arvin/microcode-shell\
@@ -10,7 +10,6 @@ pnpm --filter @arvin/microcode-types\
   --filter @arvin/microcode-designer\
   --filter @arvin/microcode-vue-simulator-renderer\
   --filter @arvin/microcode-engine\
-  
   --stream run build 
 
 echo "所有包的 build 命令执行完成"
