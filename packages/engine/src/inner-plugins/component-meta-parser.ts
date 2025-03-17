e17d6025469d1a577e2cbc2f18b83ef9cb1de424
shuwen <1243889238@qq.com>
2025-03-17 16:22:51 +0800

    build: 引擎彻底完成

--- packages/engine/src/inner-plugins/component-meta-parser.ts
+++ packages/engine/src/inner-plugins/component-meta-parser.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: shuwen 1243889238@qq.com
- * @Date: 2024-11-12 14:54:17
- * @LastEditors: shuwen 1243889238@qq.com
- * @LastEditTime: 2025-01-07 17:13:50
- * @FilePath: /microcode-engine/packages/engine/src/inner-plugins/component-meta-parser.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { IPublicModelPluginContext } from '@arvin-shu/microcode-types';
 /**
  * 组件元数据解析器插件
